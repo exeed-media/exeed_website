59466dfe19fc156609903d7a76f294cd5d336b1f
Dan <dansification@gmail.com>
2020-06-06 08:47:54 -0400

    Add Our Process Mobile animation

--- source/assets/javascripts/our_process.js
+++ source/assets/javascripts/our_process.js
@@ -8,4 +8,14 @@ window.addEventListener("load", function (event) {
     autoplay: true,
     path: 'https://assets6.lottiefiles.com/packages/lf20_CSb3dP.json'
   });
+
+  var mobile_container = document.getElementById('our_process_mobile');
+
+  var anim = lottie.loadAnimation({
+    container: mobile_container,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_Nwp8B5.json'
+  });
 });
