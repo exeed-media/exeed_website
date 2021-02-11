ea1ba7b1c6536b2fe59362a47a1fb4845731253d
jessduff <jess.tasstic@gmail.com>
2021-02-11 19:43:37 -0400

    Who we are desktop view

--- source/assets/javascripts/figures.js
+++ source/assets/javascripts/figures.js
@@ -1,17 +1,44 @@
 var halfCircle = document.querySelector('.team-half-circle');
-
-halfCircle.width = 400;
-halfCircle.height = 200;
-
 var context = halfCircle.getContext("2d");
-context.fillStyle = "#fff";
 
-let x = 200,
+let x = 0,
     y = 0,
-	  radius = 100,
+	  radius = 0,
 	  endAngle = Math.PI,
 	  startAngle = Math.PI * 2;
 
-context.arc(x, y, radius, startAngle, endAngle);
-context.stroke();
-context.fill();
+    if (window.outerWidth >= 880) {
+      halfCircle.width = 1000;
+      halfCircle.height = 500;
+      x = 820;
+      radius = 200;
+    }
+    else {
+      halfCircle.width = 400;
+      halfCircle.height = 200;
+      x = 200;
+      radius = 100;
+    }
+    context.fillStyle = "white";
+    context.arc(x, y, radius, startAngle, endAngle);
+    context.stroke();
+    context.fill();
+
+window.addEventListener('resize', function(){
+  if (window.outerWidth >= 880) {
+    halfCircle.width = 1000;
+    halfCircle.height = 500;
+    x = 820;
+    radius = 200;
+  }
+  else {
+    halfCircle.width = 400;
+    halfCircle.height = 200;
+    x = 200;
+    radius = 100;
+  }
+  context.fillStyle = "white";
+  context.arc(x, y, radius, startAngle, endAngle);
+  context.stroke();
+  context.fill();
+});
